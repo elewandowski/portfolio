1ce8069e7d3d3e0805bdb61288f5674b884ad718
Emil Lewandowski <emil.ar.lew@gmail.com>
2026-05-21 10:11:16 +0200

    Revert "remove /portfolio base path for cloudflare pages"
    This reverts commit 5560a59ee21513f047f54e694622508dc1201170.
    
    # Conflicts:
    #	src/components/ProjectCard.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://elewandowski.github.io",
+  base: "/portfolio",
   fonts: [
     {
       provider: fontProviders.local(),
