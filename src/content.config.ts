1bf2a0409a2861b29ce690413ad1564e1dcff455
Emil Lewandowski <emil.ar.lew@gmail.com>
2026-05-22 10:28:37 +0200

    add image to merger

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,15 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { file } from "astro/loaders";
 
 const projects = defineCollection({
   loader: file("src/content/projects.yml"),
+  schema: ({ image }) =>
+    z.object({
+      id: z.string(),
+      title: z.string(),
+      description: z.string(),
+      img: image().optional(),
+    }),
 });
 
 export const collections = { projects };
